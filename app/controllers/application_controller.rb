7af294a356d16199b56dfc295e49d2ca114f2c6a
takehanKosuke <kkoussuke530@icloud.com>
2019-03-01 20:52:20 +0900

    authenticate_user!を使用していない　

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, expect: %i[user_session_path, new_user_session]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  def user_sign_in?
+    if user_loged_in?
+      redirect_to :user_session_path
+    end
   end
 end
