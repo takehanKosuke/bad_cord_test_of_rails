30da7e42a775082958fcee3bd97dce329a2e37a8
takehanKosuke <kkoussuke530@icloud.com>
2019-03-01 20:28:53 +0900

    デメテルの法則を使用していない

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,15 +4,6 @@ class Article < ApplicationRecord
 
   enum status: { open: 1, close: 2 }
 
-  delegate :name,
-           to: :category,
-           prefix: true
-
-  delegate :email,
-           :name,
-           to: :user,
-           prefix: true
-
   def self.search_articles(articles, params)
     if params[:category_id]
       articles = articles.where("category_id = ?", params[:category_id])
