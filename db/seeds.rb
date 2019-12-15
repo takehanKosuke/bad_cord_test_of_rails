f3296e6d4e1ac36ca3ec292cae2dff678c3ff4d0
erm1116 <cr7sc16@gmail.com>
2019-12-16 01:49:28 +0900

    seedの冪等性

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,73 +1,36 @@
 puts 'ユーザー作成中'
-User.create(
-  [
-    {
-      name: 'kosuke',
-      email: 'test1@test.com',
-      password: '111111',
-      role: 2,
-    },
-    {
-      name: 'yuuta',
-      email: 'test2@test.com',
-      password: '111111',
-      role: 1,
-    },
-  ]
-)
+User.where(
+  id: 1,
+  name: 'kosuke',
+  email: 'test1@test.com',
+  password: '111111',
+  role: 2
+).first_or_create!
+
+User.where(
+  id: 2,
+  name: 'yuuta',
+  email: 'test2@test.com',
+  password: '111111',
+  role: 1,
+).first_or_create!
+
+
 puts '記事作成中'
-Article.create(
-  [
-    {
-      title: 'test article1',
-      body: 'test article body1',
-      user_id: 1,
-    },
-    {
-      title: 'test article2',
-      body: 'test article body2',
-      user_id: 1,
-    },
-    {
-      title: 'test article3',
-      body: 'test article body3',
-      user_id: 1,
-    },
-    {
-      title: 'test article4',
-      body: 'test article body4',
-      user_id: 1,
-    },
-    {
-      title: 'test article5',
-      body: 'test article body5',
-      user_id: 1,
-    },
-    {
-      title: 'test article6',
-      body: 'test article body6',
-      user_id: 2,
-    },
-    {
-      title: 'test article7',
-      body: 'test article body7',
-      user_id: 2,
-    },
-    {
-      title: 'test article8',
-      body: 'test article body8',
-      user_id: 2,
-    },
-    {
-      title: 'test article9',
-      body: 'test article body9',
-      user_id: 2,
-    },
-    {
-      title: 'test article10',
-      body: 'test article body10',
-      user_id: 2,
-    },
-  ]
-)
+(1..5).to_a each do |i|
+  Article.where(id: i).first_or_create!(
+    id: i,
+    title: "test article#{i}",
+    body: "test article body#{i}",
+    user_id: 1,
+  )
+end
+(6..10).to_a each do |i|
+  Article.where(id: i).first_or_create!(
+    id: i,
+    title: "test article#{i}",
+    body: "test article body#{i}",
+    user_id: 1,
+  )
+end
 puts '記事作成完了'
